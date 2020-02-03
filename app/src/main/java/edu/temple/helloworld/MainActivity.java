1f2b0d60c5f552cb8489c9b95bdc7a8ff64a6c43
tommccauley <59833800+tommccauley@users.noreply.github.com>
2020-02-03 11:01:55 -0500

    The second commit of the assignment. I have added an event listener to the button that changes the textview when clicked.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,31 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button myButton;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button);
+        txt = findViewById(R.id.myText);
+
+        myButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                txt.setText("You have clicked the button");
+            }
+
+
+        });
     }
+
+
 }
